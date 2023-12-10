805cf931a9eac4059ae96c6794b018274a556693
duncte123 <contact@duncte123.me>
2023-12-10 19:19:54 +0100

    Rework entire solution, this works like a charm.
    Although I am not sure why yet, not that good with math

--- src/main/kotlin/me/duncte123/adventofcode/DayFive.kt
+++ src/main/kotlin/me/duncte123/adventofcode/DayFive.kt
@@ -1,7 +1,6 @@
 package me.duncte123.adventofcode
 
 import me.duncte123.adventofcode.partial.AbstractSolution
-import java.util.concurrent.CountDownLatch
 
 class DayFive : AbstractSolution() {
     override fun getTestInput() =
@@ -59,96 +58,98 @@ class DayFive : AbstractSolution() {
 
         val seedsRaw = stages.removeFirst()
 
-        val seedPairs = seedsRaw.first()
+        var seedPairs = seedsRaw.first()
             .split(" ")
             .map { it.toLong() }
-            // Comment out the two lines below to switch to part one
             .chunked(2)
             .map { (source, length) ->
-                source .. (source + length)
+                source to length
             }
+            .toMutableList()
 
-        val latch = CountDownLatch(seedPairs.size)
+        // My original solution took more than 48 hours to run, I never let it finish because the power supply for my laptop was unable to keep it alive.
+        // So I'm taking inspiration from https://github.com/EdwardvanLieshout/AdventOfCode2023/blob/master/challenge5b.ts instead
+        stages.forEach { stage ->
+            val tempSeedRanges = mutableListOf<Pair<Long, Long>>()
+            stage.map { it.toRangeInput() }
+                .forEach { (destStart, sourceStart, rLen) ->
+                    for (index in 0 ..< seedPairs.size) {
+                        val (seed, sLen) = seedPairs[index]
+
+                        if (seed >= sourceStart && seed + sLen <= sourceStart + rLen) {
+                            tempSeedRanges.add(destStart + seed - sourceStart to sLen)
+                            seedPairs[index] = -1L to -1L
+                        }
+
+                        if (
+                            seed >= sourceStart &&
+                            seed < sourceStart + rLen &&
+                            seed + sLen > sourceStart + rLen &&
+                            seed != -1L
+                        ) {
+                            val start = seed
+                            val end = sourceStart + rLen
+                            val finalSeed = seed + sLen
+
+                            tempSeedRanges.add(destStart + start - sourceStart to end - start)
+                            seedPairs.add(end to finalSeed - end)
+                            seedPairs[index] = -1L to -1L
+                        }
+
+                        if (
+                            seed < sourceStart &&
+                            seed + sLen > sourceStart &&
+                            seed + sLen <= sourceStart + rLen &&
+                            seed != -1L
+                        ) {
+                            val start = sourceStart
+                            val end = seed + sLen
+                            val begin = seed
+
+                            // You know, this first equation is not making a whole lot of sense to me
+                            tempSeedRanges.add(destStart + start - sourceStart to end - start)
+                            seedPairs.add(begin to sourceStart - begin)
+                            seedPairs[index] = -1L to -1L
+                        }
+
+                        if (seed < sourceStart && seed + sLen >= sourceStart + rLen && seed != -1L) {
+                            val start = sourceStart
+                            val end = sourceStart + rLen
+                            val begin = seed
+                            val finalSeed = seed + sLen
+
+                            tempSeedRanges.add(destStart + start - sourceStart to end - start)
+                            seedPairs.add(end to finalSeed - end)
+                            seedPairs.add(begin to sourceStart - begin)
+                            seedPairs[index] = -1L to -1L
+                        }
+                    }
+                }
+
+            seedPairs = seedPairs.filter { it.first > 0 }.toMutableList()
+            seedPairs.addAll(tempSeedRanges)
+
+            println(seedPairs)
+        }
+
+        val result = seedPairs.minBy { it.first }.first
 
         println(seedPairs)
 
-        val results = mutableListOf<Long>()
-//        val seedToSoil = stages.removeFirst()
+        val incorrect = listOf(1549152L, 0L) // Highly doubt 0 is correct LMAO
 
-        seedPairs.map { stage1 ->
-            runOnVThread("RangeThread[$stage1]") {
-                // Initial conversion so that we have a list
-//                val seedToSoilRange = makeMappingRanges(seedToSoil)
-
-                val stageData = stage1.map {
-                    var res = it
-                    stages.forEach { stage ->
-                        val stageMapping = makeMappingRanges(stage)
-
-                        res = getMappedInRange(res, stageMapping)
-                        // println("[${Thread.currentThread().name}] Mapped $it to $res")
-                    }
-                    res
-                }
-
-                println(stageData)
-
-                results.add(stageData.minOf { it })
-                latch.countDown()
-            }
-        }
-
-        latch.await()
-
-        return "${results.minOf { it }}"
+        return "$result (Incorrect: ${incorrect.contains(result)})"
     }
 
-    private fun makeMappingRanges(input: List<String>): List<Pair<LongRange, LongRange>> {
-        val ranges = mutableListOf<Pair<LongRange, LongRange>>()
+    private fun String.toRangeInput(): Range {
+        val (dest, source, len) = this.split(" ").map { item -> item.toLong() }
 
-        input.forEach {
-            val (destStart, sourceStart, length) = it.split(" ").map { item -> item.toLong() }
-
-            ranges.add(
-                (sourceStart .. (sourceStart + length)) to (destStart .. (destStart + length))
-            )
-        }
-
-        return ranges
+        return Range(dest, source, len)
     }
-
-    private fun getMappedInRange(input: Long, ranges: List<Pair<LongRange, LongRange>>, result: (Long) -> Unit) = runOnVThread("MappedThread[$input]") {
-        val found = getMappedInRange(input, ranges)
-
-        result(found)
-    }
-
-    private fun getMappedInRange(input: Long, ranges: List<Pair<LongRange, LongRange>>): Long {
-        val foundRanges = ranges.filter { it.first.contains(input) }
-
-        if (foundRanges.isEmpty()) {
-            return input
-        }
-
-        foundRanges.forEach { (source, dest) ->
-            val sourceIndex = source.indexOf(input)
-
-            if (sourceIndex > -1) {
-                val found = dest.elementAtOrNull(sourceIndex)
-
-                if (found != null) {
-                    return found
-                }
-            }
-
-        }
-
-        return input
-    }
-
-    private fun runOnVThread(name: String, task: () -> Unit) = Thread.ofVirtual().name(name).start(task)
 
     private operator fun <E> List<E>.component6(): E = this[5]
     private operator fun <E> List<E>.component7(): E = this[6]
     private operator fun <E> List<E>.component8(): E = this[7]
 }
+
+data class Range(val destStart: Long, val sourceStart: Long, val length: Long) {}
