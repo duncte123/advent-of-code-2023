9e5ea55e48843fdecd18705ed9685c43105f7312
duncte123 <contact@duncte123.me>
2023-12-12 09:45:32 +0100

    Fail being clever

--- src/main/kotlin/me/duncte123/adventofcode/DayEight.kt
+++ src/main/kotlin/me/duncte123/adventofcode/DayEight.kt
@@ -1,20 +1,22 @@
 package me.duncte123.adventofcode
 
 import me.duncte123.adventofcode.partial.AbstractSolution
+import java.util.concurrent.CountDownLatch
 
 class DayEight : AbstractSolution() {
-    private val directionRegex = "(?<key>[A-Z]{3}) = \\((?<left>[A-Z]{3}), (?<right>[A-Z]{3})\\)".toRegex()
+    private val directionRegex = "(?<key>[A-Z0-9]{3}) = \\((?<left>[A-Z0-9]{3}), (?<right>[A-Z0-9]{3})\\)".toRegex()
 
     override fun getTestInput() =
-        "RL\n" +
+        "LR\n" +
         "\n" +
-        "AAA = (BBB, CCC)\n" +
-        "BBB = (DDD, EEE)\n" +
-        "CCC = (ZZZ, GGG)\n" +
-        "DDD = (DDD, DDD)\n" +
-        "EEE = (EEE, EEE)\n" +
-        "GGG = (GGG, GGG)\n" +
-        "ZZZ = (ZZZ, ZZZ)"
+        "11A = (11B, XXX)\n" +
+        "11B = (XXX, 11Z)\n" +
+        "11Z = (11B, XXX)\n" +
+        "22A = (22B, XXX)\n" +
+        "22B = (22C, 22C)\n" +
+        "22C = (22Z, 22Z)\n" +
+        "22Z = (22B, 22B)\n" +
+        "XXX = (XXX, XXX)"
 
     override fun run(input: String): String {
         val instructions = mutableMapOf<String, Instruction>()
@@ -37,8 +39,25 @@ class DayEight : AbstractSolution() {
         println(direction)
         println(instructions)
 
+        val endsInA = instructions.keys.filter { it.endsWith('A') }
+        val latch = CountDownLatch(endsInA.size)
+        val steps = 0
+
+        endsInA.forEach { startKey ->
+            followPath(startKey, direction, instructions) {
+                println("Steps: $it")
+                latch.countDown()
+            }
+        }
+
+        latch.await()
+
+        return "$steps"
+    }
+
+    private fun followPath(startKey: String, direction: String, instructions: Map<String, Instruction>, stepsCallback: (Int) -> Unit) = runOnVirtual(startKey) {
         var steps = 0
-        var curInstruction = "AAA"
+        var curInstruction = startKey
 
         while (true) {
             val instruction = instructions[curInstruction]!!
@@ -49,22 +68,24 @@ class DayEight : AbstractSolution() {
             steps++
 
             if (stepToTake == 'L') {
-                if (instruction.left == "ZZZ") {
+                if (instruction.left.endsWith('Z')) {
+                    stepsCallback(steps)
                     break
                 }
 
                 curInstruction = instruction.left
             } else {
-                if (instruction.right == "ZZZ") {
+                if (instruction.right.endsWith('Z')) {
+                    stepsCallback(steps)
                     break
                 }
 
                 curInstruction = instruction.right
             }
         }
-
-        return "$steps"
     }
+
+    private fun runOnVirtual(start: String, task: () -> Unit) = Thread.ofVirtual().name("SearchTask[$start]").start(task)
 }
 
 data class Instruction(val left: String, val right: String)
